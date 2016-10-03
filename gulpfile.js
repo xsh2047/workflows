b9b839055bc2cf92aac1b0119b73ef349467d967
Xavier Bryson <zabieru96@hotmail.com>
2016-10-03 04:00:55 -0500

    Created Default Task to run other tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeScripts = ['components/coffee/desc.coffee'];
@@ -9,6 +10,7 @@ var javaScripts = [
     'components/scripts/desc.js',
     'components/scripts/main.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
     //gutil.log('Hello World');
@@ -24,3 +26,16 @@ gulp.task('js', function(){
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function(){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('default', ['coffee', 'js', 'compass']);
